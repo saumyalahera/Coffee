fd2199cd7056afd3db227db166f5b2b9203d3b4d
Saumya Lahera <saumyalahera@gmail.com>
2021-07-15 17:14:52 -0500

    Solved Architecture Issues
    Project was not building on any simulator and the only solution was to make it run on m1 simulators

--- Coffee/ViewController.swift
+++ Coffee/ViewController.swift
@@ -6,14 +6,57 @@
 //
 
 import UIKit
+import GooglePlaces
 
 class ViewController: UIViewController {
 
+    //AIzaSyCmDuyvI3rh4rxSGVLSCZPymJHsXKi20sk
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+//MARK: - Search Destination
+    @IBAction func searchDestination(_ sender: Any) {
+        
+        let autocompleteController = GMSAutocompleteViewController()
+            autocompleteController.delegate = self
 
+            // Specify the place data types to return.
+            let fields: GMSPlaceField = GMSPlaceField(rawValue: UInt(GMSPlaceField.name.rawValue) |
+                                                        UInt(GMSPlaceField.placeID.rawValue))
+            autocompleteController.placeFields = fields
 
+            // Specify a filter.
+            let filter = GMSAutocompleteFilter()
+            filter.type = .address
+            autocompleteController.autocompleteFilter = filter
+
+            // Display the autocomplete view controller.
+            present(autocompleteController, animated: true, completion: nil)
+    }
+    
+}
+
+extension ViewController:GMSAutocompleteViewControllerDelegate {
+    
+    func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+        print("Place name: \(place.name ?? "j")")
+        print("Place ID: \(place.placeID)")
+        print("Place attributions: \(place.attributions)")
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
+        print("Error: ", error.localizedDescription)
+    }
+    
+    func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+        print("")
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
 }
 
---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '14.4'
+
+target 'Coffee' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Coffee
+
+  target 'CoffeeTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'CoffeeUITests' do
+    # Pods for testing
+  end
+
+  pod 'GooglePlaces'
+
+end
