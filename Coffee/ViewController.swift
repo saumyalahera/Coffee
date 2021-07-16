0e1b5ae9349b93bdfd6cd44489b8f66c1da9f05a
Saumya Lahera <saumyalahera@gmail.com>
2021-07-16 00:57:58 -0500

    Code cleaning

--- Coffee/ViewController.swift
+++ Coffee/ViewController.swift
@@ -12,37 +12,51 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
 //MARK: - Search Destination
     @IBAction func searchDestination(_ sender: Any) {
         
-        let autocompleteController = GMSAutocompleteViewController()
-            autocompleteController.delegate = self
-       
+        guard let autocompleteController = self.setupGMSAutocomplete(delegate: self) else {
+            return
+        }
 
-            // Specify the place data types to return.
-            let fields: GMSPlaceField = GMSPlaceField(rawValue: UInt(GMSPlaceField.name.rawValue) |
-                                                        UInt(GMSPlaceField.placeID.rawValue))
-            autocompleteController.placeFields = fields
-
-            // Specify a filter.
-            let filter = GMSAutocompleteFilter()
-            filter.type = .address
-            autocompleteController.autocompleteFilter = filter
-
-            // Display the autocomplete view controller.
-            present(autocompleteController, animated: true, completion: nil)
+    // Display the autocomplete view controller
+        present(autocompleteController, animated: true, completion: nil)
     }
     
 }
 
+/**This extension is for place auto complete. GMSAutocompleteViewController has a search bar and a table view that will displaty results.*/
 extension ViewController:GMSAutocompleteViewControllerDelegate {
     
+//MARK: - Setup Autocomplete View controller
+    func setupGMSAutocomplete(delegate: GMSAutocompleteViewControllerDelegate) -> GMSAutocompleteViewController? {
+        
+        let autocompleteController = GMSAutocompleteViewController()
+        autocompleteController.delegate = delegate
+
+    // Specify the place data types to return
+        let fields: GMSPlaceField = GMSPlaceField(rawValue: UInt(GMSPlaceField.name.rawValue) | UInt(GMSPlaceField.placeID.rawValue))
+        autocompleteController.placeFields = fields
+
+    // Specify a filter
+        let filter = GMSAutocompleteFilter()
+        filter.type = .address
+        autocompleteController.autocompleteFilter = filter
+        
+        return autocompleteController
+    }
+    
+    
+//MARK: - Delegate Methods
     func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
-        print("Place name: \(place.name ?? "j")")
-        print("Place ID: \(place.placeID)")
-        print("Place attributions: \(place.attributions)")
+        
+        guard let placeName = place.name, let placeId = place.placeID else {
+            return
+        }
+        print("Place name: \(placeName)")
+        print("Place ID: \(placeId)")
         dismiss(animated: true, completion: nil)
     }
     
@@ -54,7 +68,5 @@ extension ViewController:GMSAutocompleteViewControllerDelegate {
         print("")
         dismiss(animated: true, completion: nil)
     }
-    
-    
 }
 
